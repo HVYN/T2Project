99917f6ac0c1059985a0e297260b496e379f7aad
Unicoranium <blrunyon@gmail.com>
2023-02-16 19:48:12 -0600

    reformat url parsing

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const fs = require('fs');
 const host = 'localhost';
 const port = 8000;
 
+const ASSETS = {
+	'bootstrap.min.css': { url: '/node_modules/bootstrap/dist/css/bootstrap.min.css', type: 'text/css' },
+	'bootstrap.min.css.map': { url: '/node_modules/bootstrap/dist/css/bootstrap.min.css.map', type: 'application/json' },
+	'bootstrap.min.js': { url: '/node_modules/bootstrap/dist/js/bootstrap.min.js', type: 'text/javascript' },
+	'bootstrap.min.js.map': { url: '/node_modules/bootstrap/dist/js/bootstrap.min.js.map', type: 'application/json' },
+};
+
 function createPage(content) {
 	let header = fs.readFileSync(__dirname + '/header.html', 'utf8');
 	let footer = fs.readFileSync(__dirname + '/footer.html', 'utf8');
@@ -14,29 +21,15 @@ function createPage(content) {
 
 const requestListener = function (req, res) {
 	let url = req.url.substring(1);
-	console.log(url);
-
-	if (url == 'bootstrap.min.css') {
-		fs.readFile(__dirname + '/node_modules/bootstrap/dist/css/bootstrap.min.css', (err, data) => {
+	if (url in ASSETS) {
+		let asset = ASSETS[url];
+		fs.readFile(__dirname + asset.url, (err, data) => {
 			if (err) {
 				res.writeHead(404);
 				res.end(JSON.stringify(err));
 				return;
 			}
-			res.setHeader('Content-Type', 'text/css');
-			res.writeHead(200);
-			res.end(data);
-		});
-		return;
-	}
-	if (url == 'bootstrap.min.js') {
-		fs.readFile(__dirname + '/node_modules/bootstrap/dist/js/bootstrap.min.js', (err, data) => {
-			if (err) {
-				res.writeHead(404);
-				res.end(JSON.stringify(err));
-				return;
-			}
-			res.setHeader('Content-Type', 'text/css');
+			res.setHeader('Content-Type', asset.type);
 			res.writeHead(200);
 			res.end(data);
 		});
@@ -54,7 +47,9 @@ const requestListener = function (req, res) {
 			res.writeHead(200);
 			res.end(createPage(data));
 		});
+		return;
 	}
+	console.log(`Unknown url: "${url}"`);
 };
 
 const server = http.createServer(requestListener);
