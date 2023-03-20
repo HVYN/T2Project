1b6d1b79a82102fbcd30d99dcd469b7676b67340
John Zhao <gmaster234@gmail.com>
2023-03-20 06:09:26 -0500

    First Step into Express.js, and Pug.js
    I'm starting to play with Express.js and Pug.js a little, mostly to get familiar with these two frameworks/tools in order to better align ourselves with the professor's tutorials.
    
    Some notes: I couldn't upload what I wanted from 'node_modules'; the dependencies you will want to install for this are Express, and Pug.
    
    The images folder contains most of the images we are using for the homepage carousel, and the stock photo.
    
    I have yet to create usernames and passwords for you guys for MongoDB, let me know when you try to access the database so I can get that going instead of using my credentials.
    
    So far, the tutors page now will display all tutors from the MongoDB collection ('tutor-application' -> 'tutors').
    
    If something I didn't cover above springs up, let me know (Discord).

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
+// MAIN .JS FILE
+
+//	TEST: USING EXPRESS
+const express = require('express');
+const expressApp = express();
+
+//	TEST: USING MONGODB (Mongoose)
+// import { mongoInit } from './mongo.js';
+const mongo = require('./mongo');
+
 const http = require('http');
 const fs = require('fs');
+const { create } = require('domain');
 
+//	ADDRESS INFORMATION
 const host = 'localhost';
 const port = 8000;
 
+/*
+//	ALL OF OUR ASSETS ARE HERE, INCLUDING IMAGES
 const ASSETS = {
 	'bootstrap.min.css': { url: '/node_modules/bootstrap/dist/css/bootstrap.min.css', type: 'text/css' },
 	'bootstrap.min.css.map': { url: '/node_modules/bootstrap/dist/css/bootstrap.min.css.map', type: 'application/json' },
 	'bootstrap.min.js': { url: '/node_modules/bootstrap/dist/js/bootstrap.min.js', type: 'text/javascript' },
 	'bootstrap.min.js.map': { url: '/node_modules/bootstrap/dist/js/bootstrap.min.js.map', type: 'application/json' },
-	'logo.png' : { url: '/logo.png', type: 'image/png'},
+	'logo.png' : { url: '/images/logo.png', type: 'image/png'},
 	'favicon.ico': { url: '/favicon_io/favicon.ico', type: 'image/x-icon' },
-	'stockphoto.png' : { url: '/stockphoto.png', type: 'image/png'},
-	'grayBg.png' : {url: '/grayBg.png', type: 'image/png'},
-	'orangeBg.png' : {url: '/orangeBg.png', type: 'image/png'}
+	'stockphoto.png' : { url: '/images/stockphoto.png', type: 'image/png'},
+	'grayBg.png' : {url: '/images/grayBg.png', type: 'image/png'},
+	'orangeBg.png' : {url: '/images/orangeBg.png', type: 'image/png'},
+	'custom.css' : {url: '/custom.css', type: 'text/css'},
+	'transparent.png': {url: '/transparent.png', type: 'image/png'}
 };
+*/
 
-function createPage(content) {
+//	CREATEPAGE ALWAYS LOADS FOOTER AND HEADER
+//	THE ACTUAL PAGE WILL BE LOADED AS NEEDED
+function createPage(content) 
+{
 	let header = fs.readFileSync(__dirname + '/header.html', 'utf8');
 	let footer = fs.readFileSync(__dirname + '/footer.html', 'utf8');
-	let tutors = fs.readFileSync(__dirname + '/tutors.html', 'utf-8')
-	let page = `<!DOCTYPE html><html>${header}${content}${footer}</html>`;
+
+	let page = `<!DOCTYPE html><html>
+				${header}${content}${footer}
+				</html>`;
 	page = page.replaceAll('{{dir}}', __dirname);
 	return page;
 }
 
+//	TEST: DISPLAY TUTORS FROM /GET REQUEST
+function createTutorPage(content)
+{
+	let header = fs.readFileSync(__dirname + '/header.html', 'utf8');
+	let footer = fs.readFileSync(__dirname + '/footer.html', 'utf8');
+
+	let page = `<!DOCTYPE html><html>
+				${header}${content}${footer}
+				</html>`;
+	page = page.replaceAll('{{dir}}', __dirname);
+	return page;
+}
+
+/*
+//	WHAT THE USER CLICKS ON, THEIR 'REQUESTS' ARE SENT HERE TO BE
+//	INTERPRETED, AND REDIRECTED
 const requestListener = function (req, res) {
 	let url = req.url.substring(1);
+
+	//	IF THE PAGE NEEDS SOMETHING FORM ASSETS (LIKE AN IMAGE)
 	if (url in ASSETS) {
 		let asset = ASSETS[url];
 		fs.readFile(__dirname + asset.url, (err, data) => {
@@ -42,6 +82,7 @@ const requestListener = function (req, res) {
 		return;
 	}
 
+	//	IF USER IS TRYING TO REACH TUTORS PAGE
 	if (url == 'tutors.html')
 	{
 		fs.readFile(__dirname + '/tutors.html', (err, data) => {
@@ -58,6 +99,8 @@ const requestListener = function (req, res) {
 		return;
 	}
 
+	//	HOMEPAGE; IF NO SPECIFIC .HTML PAGE, THEN IT'S
+	//	ALSO HOMEPAGE
 	if (url == '' || url == 'index.html') {
 		fs.readFile(__dirname + '/index.html', (err, data) => {
 			if (err) {
@@ -72,10 +115,74 @@ const requestListener = function (req, res) {
 		return;
 	}
 
+	//	IF AN UNKNOWN RESOURCE IS REQUESTED, PRINT
+	//	TO CONSOLE
 	console.log(`Unknown url: "${url}"`);
 };
+*/
 
+//	TEST: SET TEMPLATE FILE DIRECTORY TO views
+expressApp.set('views', './views');
+
+//	TEST: SET VIEW ENGINE TO pug
+expressApp.set('view engine', 'pug');
+
+//	TEST: EXPRESS STUFF
+expressApp.listen(port, host, () => {
+	console.log(`EXPRESS RUNNING: http://${host}:${port}`)
+});
+
+//	TEST: USING EXPRESS - LOAD STATIC FILES (CSS)
+expressApp.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+expressApp.use(express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+expressApp.use(express.static(__dirname + '/images'));
+//	Need to fix favicon not appearing 
+expressApp.use(express.static(__dirname + '/favicon_io'));
+expressApp.use(express.static(__dirname + '/css'));
+
+// TEST: EXPRESS - HANDLE HOMEPAGE
+expressApp.get('/', (req, res) => {
+	fs.readFile(__dirname + '/index.html', (err, data) => {
+		if(err) {
+			res.status(404).end(JSON.stringify(err));
+		}	
+
+		res.setHeader('Content-Type', 'text/html');
+		res.status(200).end(createPage(data));
+	})
+})
+
+// TEST: EXPRESS - HANDLE TUTORS
+expressApp.get('/tutors', (req, res) => {
+
+	//	GET ALL DATABASE ITEMS (tutor-application)
+	const databaseItems = mongo.getAllTutors();
+
+	databaseItems.then(function(result) {
+		console.log(result);
+
+		//	DISPLAY PAGE using pug
+		res.render('tutors', { tutors: result});
+
+		/*
+		//	DISPLAY PAGE using PREVIOUS METHOD (js, HTML)
+		fs.readFile(__dirname + '/tutors.html', (err, data) => {
+			if(err) 
+			{
+				res.status(404).end(JSON.stringify(err));
+			}	
+
+			res.setHeader('Content-Type', 'text/html');
+			res.status(200).end(createPage(data));
+		});
+		*/
+	});
+});
+
+/*
 const server = http.createServer(requestListener);
+
 server.listen(port, host, () => {
 	console.log(`Server is running on http://${host}:${port}`);
 });
+*/
---
+++ mongo.js
@@ -0,0 +1,59 @@
+/*
+    03/19/2023
+    Separate .js file that handles MongoDB stuff
+*/
+
+//	TEST: MONGODB STUFF
+const {MongoClient} = require('mongodb');
+
+//	MONGODB CONNECTION STRING (URI)
+const mongoConnection = "mongodb+srv://gmaster234:8OvIsqflM7POC5Dx@t2-project-cluster.yya5asd.mongodb.net/?retryWrites=true&w=majority"
+
+const mongoClient = new MongoClient(mongoConnection)
+
+//  EXPORTING FUNCTION
+//	TEST: MONGODB STUFF
+async function mongoInit()
+{
+	//	CONNECT TO THE MONGODB CLUSTER
+	try
+	{
+		await mongoClient.connect().then(() => {
+			console.log('SUCCESSFULLY CONNECTED TO DATABASE!');
+		});
+	}
+	catch (err)
+	{
+		console.error(err);
+	}
+	finally
+	{
+		await mongoClient.close();
+	}
+
+}
+
+//  TEST: DISPLAY TUTOR-APPLICATION DATABASE
+async function getAllTutors()
+{
+    const database = await mongoClient.db('tutor-application')
+        .collection('tutors')
+        .find();
+
+    const databaseItems = await database.toArray();
+
+    return databaseItems;
+}
+
+//	TEST: MONGODB STUFF - LIST ALL DATABASES
+async function listDatabases(client)
+{
+	databasesList = await client.db().admin().listDatabases();
+
+	console.log(databasesList.databases);
+
+}
+
+//  EXPORT THIS SO IT'S USABLE BY index.js
+exports.mongoInit = mongoInit;
+exports.getAllTutors = getAllTutors;
